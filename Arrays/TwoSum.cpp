3efb95e8e5d5c51d978e8f550b994555e43daba2
Swanand Joshi <swanandj2004@gmail.com>
2025-09-21 12:12:16 +0530

    Update TwoSum.cpp

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -1,1 +1,31 @@
+#include <vector>
+#include <iostream>
+using namespace std;
 
+void twosum(vector<int>nums, vector<int>& index, int target) {
+  for(int i=0; i<nums.size(); i++) {
+    for(int j=i+1; j<nums.size(); j++) {
+      if(nums.at(i)+nums.at(j)==target) {
+        index.push_back(i);
+        index.push_back(j);
+        break;
+      }
+    }
+  }
+}
+
+int main() {
+  vector<int>nums;
+  vector<int>index;
+  int target=9;
+  nums.push_back(2);
+  nums.push_back(7);
+  nums.push_back(11);
+  nums.push_back(15);
+  twosum(nums, index, target);
+  for(int i=0; i<index.size(); i++) {
+    cout << index.at(i) << " ";
+  }
+
+  return 0;
+}
